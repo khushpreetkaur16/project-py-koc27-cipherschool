080b6955cee044324a69e352765be3a8434f947c
khushpreetkaur16 <115393297+khushpreetkaur16@users.noreply.github.com>
2022-12-30 21:18:48 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,18 @@
+
+# Multiplication-Table-Python
+# Multiplication-Table-Python
+
+n = int(input("Enter number"))
+
+class multiplication:
+    attribute = n
+    def function(self):
+        for i in range(2, self.attribute+1):
+            for j in range(1, 11):
+                c = i*j
+                print(i, "*", j, "=", c)
+            print("\n")
+
+m1 = multiplication()
+
+m1.function()
